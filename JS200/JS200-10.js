5a8145a0a381360c50cc84fbcac986b2a5f6cdba
sang woo <tooil1729@gmail.com>
2023-02-20 16:03:41 +0900

    [sina] JS200 10~13번 학습

---
+++ JS200/JS200-10.js
@@ -0,0 +1,15 @@
+// JS 200제 조건문 배우기 - if (입문 10)
+
+let result = true;
+if(result) console.log('result 가 참 입니다.');
+if (!result) console.log('실행되지 않습니다.')
+if(result) {
+  console.log('result의 결과');
+  console.log('>> 참 입니다')
+}
+
+
+// ex
+
+const result2 = '?';
+if(result2) console.log(result2);
---
+++ JS200/JS200-11.js
@@ -0,0 +1,37 @@
+
+// JS 200제 조건문 배우기 - if, else if, else (입문 11)
+
+const number = 4;
+if (number ==1) {
+  console.log('number는 1 입니다');
+} else if (number == 2) {
+  console.log('number는 2 입니다');
+} else if (number == 3) {
+  console.lo('number는 3 입니다');
+} else {
+  console.log('number는 1, 2, 3 중 해당되는 것이 없습니다.');
+}
+
+/* 
+if (표현식1) {
+  명령문1
+} else if (표현식2) {
+  명령문2
+} else if (표현식3) {
+  명령문3
+}  else {
+  명령문 4
+}
+*/
+
+// ex
+
+const result2 = '?';
+if(result2) console.log(result2);
+
+if (result2 > 100) {
+  console.log('true?');
+} else if (result2 == String) {
+  console.log('true?') 
+} else (result2) 
+  console.log('false?')
