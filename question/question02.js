5fff799c486c34f7fd1f197bb24a2652c9f36d3c
sang woo <tooil1729@gmail.com>
2023-02-22 19:22:26 +0900

    [sina] JavaScript 문제 풀이

--- question/question02.js
+++ question/question02.js
@@ -23,53 +23,24 @@ const solution = str => {
     plum : '자두',
     tangerine : '귤',
     mango : '망고'
-  };
-
-  const banana = fruit.banana;
-  const plum = fruit.plum;
-  const tangerine = fruit.tangerine;
-  const mango = fruit.mango;
-
-  
-  // const banana = fruit.name === '바나나'
-  // const plum = item.filter((plum) => fruit['자두']);
-  // const tangerine = item.find(element.name === '귤');
-  // const mango = item.find(element.name === '망고');
-
-  if (banana === str) {
-    console.log('바나나 상품은 5만원')
-  } else if (plum === str) {
-    console.log('자두가 안보임')
-  } else if (tangerine === str) {
-    console.log('귤 상품이 없습니다')
-  } else if (mango === str) {
-    console.log('망고는 망고')
-  } else {
-    console.log('사장님 과일이 안보입니다.')
   }
- 
-  // if(str.banana ==='바나나') {
-  //   const banana = str.banana
-  //   console.log('바나나 상품은 1500원입니다.')
-  //   return banana
-  // } else if (str.plum ==='자두') {
-  //   const plum = str.plum
-  //   console.log('바나나 상품은 1200원입니다.')
-  //   return plum;
-  // } else if (str.tangerine === '귤') {
-  //   const tangerine = str.tangerine
-  //   console.log('귤 상품이 없습니다.')
-  //   return tangerine
-  // } else if ('망고') {
-  //   const mango = str.mango
-  //   console.log('망고 상품이 없습니다.')
-  //   return mango
-  // } else {
-  //   console.log('찾는 과일이 없습니다.')
-  // }
 
+
+  if (fruit.banana === str) {
+    console.log('바나나 상품은 1500원입니다')
+  } else if(fruit['plum'] === str) {
+    console.log('바나나 상품은 1200원입니다.')
+  } else if(fruit['tangerine'] === str) {
+    console.log('귤 상품이 없습니다.')
+  } else if (fruit.mango === str) {
+    console.log('망고 상품이 없습니다.')
+  } else {
+    console.log('찾으시는 과일이 없습니다')
+  }
+  
   return str
 }
+
 
 console.log(solution('바나나')) // 바나나 상품은 1500원입니다.
 console.log(solution('자두')) // 바나나 상품은 1200원입니다.
