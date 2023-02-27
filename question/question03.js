5a6c7971bf4d009634f673e54abdfd41365ccba8
sang woo <tooil1729@gmail.com>
2023-02-27 15:28:44 +0900

    [sina] JavaScript 문제 풀이

--- question/question03.js
+++ question/question03.js
@@ -1,26 +1,15 @@
 // userInfo에 있는 ableSkill에 skill을 넣을 수 있는 함수는 userInfo안에 만들어보세요
 
 const userInfo = {
-name:"sina",
-ableSkill:{},
-setAbleSkill : function () {
-  this.ableSkill = {
-    JavaScript : '10점'
+  name:"sina",
+  ableSkill:{},
+  setAbleSkill : function (key) {
+    this.ableSkill = key;
   }
 }
-}
-userInfo.setAbleSkill();
 
+userInfo.setAbleSkill({"javascript":"10점"});
 console.log(userInfo.ableSkill);
 
-const sina = userInfo.ableSkill;
-console.log(Object
-  .values(sina)
-  .toString()
-  );
-
-  // userInfo.setAbleSkill({
-// "javascript":"10점"
-// })
 // userInfo.ableSkill
 // { javascript: '10점' } => 이렇게 콘솔에 찍히면 됩니다.
